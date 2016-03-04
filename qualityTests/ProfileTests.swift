733bd5832ead17d6a67c168c643b31397dccb56e
Marcus Kuquert <marcus.kuquert@icloud.com>
2016-03-04 13:50:33 -0300

    Removed unused schemas, fixed testeExample

--- qualityTests/ProfileTests.swift
+++ qualityTests/ProfileTests.swift
@@ -11,13 +11,8 @@ import XCTest
 
 class ProfileTests: XCTestCase {
     
-    var train: Trainer?
-    
     override func setUp() {
         super.setUp()
-        APIManager.login(user: "ash", password: "mistyS2") { (success, error, trainer) -> () in
-            self.train = trainer
-        }
     }
     
     override func tearDown() {
@@ -25,24 +20,16 @@ class ProfileTests: XCTestCase {
     }
     
     func testExample() {
-        print("passou")
-//        weak var expectation = expectationWithDescription("loginFunction")
-//        
-//        APIManager.login(user: "ash", password: "mist") { (success, error, trainer) -> () in
-//            expectation?.fulfill()
-//        }
+        weak var expectation = expectationWithDescription("loginFunction")
         
-//        waitForExpectationsWithTimeout(5) { (error) -> Void in
-//            if error == nil{
-                XCTAssertEqual(self.train!.onHandPokemons?.count, 6)
-//            }
-//        }
+        APIManager.login(user: "ash", password: "mist") { (success, error, trainer) -> () in
+            expectation?.fulfill()
+        }
+        
+        waitForExpectationsWithTimeout(5) { (error) -> Void in
+            if error == nil{
+                XCTAssertEqual(APIManager.sharedInstance.currentTrainer?.onHandPokemons?.count, 6)
+            }
+        }
     }
-//    
-//    func testPerformanceExample() {
-//        // This is an example of a performance test case.
-//        self.measureBlock {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
 }
